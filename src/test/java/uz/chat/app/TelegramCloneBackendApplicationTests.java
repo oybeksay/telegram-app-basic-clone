09cce9c93ade0b69ba5b543f21991e26603bfafd
oybek <oybeks.pro@gmail.com>
2025-01-10 17:47:25 +0500

    initial-commit

---
+++ src/test/java/uz/chat/app/TelegramCloneBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package uz.chat.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TelegramCloneBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
