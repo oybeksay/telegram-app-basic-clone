913dca1b94eeed15154f72a662784b7ae7cbe18d
oybek <oybeks.pro@gmail.com>
2025-01-15 11:55:04 +0500

    write full security

--- src/main/java/uz/chat/app/controller/TokenController.java
+++ src/main/java/uz/chat/app/controller/TokenController.java
@@ -3,6 +3,7 @@ package uz.chat.app.controller;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.web.bind.annotation.*;
@@ -24,21 +25,24 @@ public class TokenController {
     public ResponseEntity<Map<String,String>> login(@RequestParam String username,
                                                     @RequestParam String password) {
         authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(username, password));
-        Map<String, String> tokens = Map.of(
-                "refreshToken", jwtUtil.generateRefreshToken(username), "accessToken", jwtUtil.generateAccessToken(username)
+        Map<String,String> tokens = Map.of(
+                "refreshToken", jwtUtil.generateRefreshToken(username),
+                "accessToken", jwtUtil.generateAccessToken(username)
         );
         return ResponseEntity.ok(tokens);
     }
+
+    @GetMapping("/refreshAccessToken")
+    public ResponseEntity<Map<String,String>> createAccessToken(@RequestParam String refreshToken) {
+        return ResponseEntity.ok(Map.of("accessToken", jwtUtil.refreshAccessToken(refreshToken)));
+    }
+
 
     @PostMapping("/sendActivationCode/{username}")
     public ResponseEntity<UserOtp> sendActivationCode(@PathVariable String username){
         return ResponseEntity.status(HttpStatus.CREATED).body(userOtpService.generateUserOtp(username));
     }
 
-    @GetMapping("/refreshToken")
-    public ResponseEntity<Map<String,String>> createAccessToken(@RequestParam String refreshToken) {
-        return ResponseEntity.ok(Map.of("refreshToken", jwtUtil.generateRefreshToken(refreshToken)));
-    }
 
     @GetMapping("/activation/{token}")
     public ResponseEntity<String> activationAccount(@PathVariable String token) {
